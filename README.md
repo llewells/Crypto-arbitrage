ccabdabe208c27f7e7d2f91820057186a5a1ad96
Simon Llewellyn <llewells86@gmail.com>
2024-08-11 11:49:26 +0100

    init repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Crypto Arbitrage 
+## AUTHOR
+Simon Llewelle - based upon (https://medium.com/@gk_/crypto-arbitrage-trading-on-binance-12dcb4d17b30)
+## DESCRIPTION
+Testing the concept of crupto arbitrage on different excahnges
---
+++ arbitrage_bot.py
@@ -0,0 +1,10 @@
+"""
+NAME
+    Arbitrage_bot.py
+
+AUTHOR
+    Simon Llewelle - based upon (https://medium.com/@gk_/crypto-arbitrage-trading-on-binance-12dcb4d17b30)
+
+DESCRIPTION
+    Testing the concept of crupto arbitrage on different excahnges
+"""
